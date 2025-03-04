e7b0f534b05e8ce7a8cddeef88f0f0eea97f9f48
Derek <bak_trak_09@yahoo.com>
2025-03-03 21:39:52 -0500

    Fixed circular import by moving models import after db.init_app(app)

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ import enum
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from extensions import db
-from app import db
+
+db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
