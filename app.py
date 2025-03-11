8fea54a92a7a48acb1eadbb719bb8327ce8e3204
Derek <bak_trak_09@yahoo.com>
2025-03-10 21:16:03 -0400

    Fix circular import by moving db to extensions.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ else:
 
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=20)
 app.config["SESSION_FILE_DIR"] = "./flask_session"  # Ensures session data is saved
